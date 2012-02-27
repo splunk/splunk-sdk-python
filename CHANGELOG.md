aa360353f9e48054ab424b637d8bd653cd8e06ef
Brad Lovering <blovering@splunk.com>
2012-02-27 12:55:08 -0800

    Improve robustness of test_client.py#test_apps.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Splunk Python SDK Changelog
 
+## develop
+
+* Move all classes in the core library modules to new-style
+* Rename Job.setpriority to Job.set_priority
+* Rename Job.setttl to Job.set_ttl
+
+* Improvements to unit tests
+
 ## v0.1.0a
 
 * Fix a bug in the dashboard example
--- tests/runtests.py
+++ tests/runtests.py
@@ -13,8 +13,8 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-#
-# Run all the Splunk Python SDK tests
+
+"""Runs all the Splunk Python SDK unit tests."""
 
 import os
 

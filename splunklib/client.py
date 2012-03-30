4bcb4c79652d0bc6624b91f44ce6893c16c7aec1
Brad Lovering <blovering@splunk.com>
2012-03-29 19:22:49 -0700

    Update copyright banners.

--- splunklib/client.py
+++ splunklib/client.py
@@ -1,4 +1,4 @@
-# Copyright 2011 Splunk, Inc.
+# Copyright 2011-2012 Splunk, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"): you may
 # not use this file except in compliance with the License. You may obtain

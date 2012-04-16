3c54852cf8a0e9475e477dc990f7dc1a04547e88
Brad Lovering <blovering@splunk.com>
2012-04-15 22:08:18 -0700

    Change theme to haiku.

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+
+# agogo, default, epub, haiku, nature, pyramid, scrolls, sphinxdoc, traditional
+html_theme = 'haiku' 
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

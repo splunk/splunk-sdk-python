9d63d7d1d81a57d939b1602924db4398aaf5c7ca
Frederick Ross <fross@splunk.com>
2012-08-13 12:27:21 -0700

    Reworked test_service.py.
    service.parse will need to be rewritten. It's useless right now.

--- splunklib/client.py
+++ splunklib/client.py
@@ -107,6 +107,29 @@ XNAME_CONTENT = XNAMEF_ATOM % "content"
 
 MATCH_ENTRY_CONTENT = "%s/%s/*" % (XNAME_ENTRY, XNAME_CONTENT)
 
+capabilities = record({k:k for k in [
+            "admin_all_objects", "change_authentication", 
+            "change_own_password", "delete_by_keyword",
+            "edit_deployment_client", "edit_deployment_server",
+            "edit_dist_peer", "edit_forwarders", "edit_httpauths",
+            "edit_input_defaults", "edit_monitor", "edit_roles",
+            "edit_scripted", "edit_search_server", "edit_server",
+            "edit_splunktcp", "edit_splunktcp_ssl", "edit_tcp",
+            "edit_udp", "edit_user", "edit_web_settings", "get_metadata",
+            "get_typeahead", "indexes_edit", "license_edit", "license_tab",
+            "list_deployment_client", "list_forwarders", "list_httpauths",
+            "list_inputs", "request_remote_tok", "rest_apps_management",
+            "rest_apps_view", "rest_properties_get", "rest_properties_set",
+            "restart_splunkd", "rtsearch", "schedule_search", "search",
+            "use_file_operator"]})
+
+
+class NoSuchUserException(Exception):
+    pass
+
+class NoSuchApplicationException(Exception):
+    pass
+
 class IllegalOperationException(Exception):
     pass
 

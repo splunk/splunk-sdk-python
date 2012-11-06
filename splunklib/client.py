cf0227ba6bf4abfe61bca7c5f016e363e9783047
Frederick Ross <fross@splunk.com>
2012-11-06 10:16:23 -0800

    Add comments on why we put in _BaseService.

--- splunklib/client.py
+++ splunklib/client.py
@@ -285,6 +285,12 @@ def connect(**kwargs):
     """
     return Service(**kwargs).login()
 
+# In preparation for adding Storm support, we added an
+# intermediary class between Service and Context. Storm's
+# API is not going to be the same as enterprise Splunk's
+# API, so we will derive both Service (for enterprise Splunk)
+# and StormService for (Splunk Storm) from _BaseService, and
+# put any shared behavior on it.
 class _BaseService(Context):
     pass
 

b8f505b787edbbbf9f32a311cdbc0d5e7c4da290
Shakeel Mohamed <smohamed@splunk.com>
2013-07-23 09:32:56 -0700

    Add a space after # for comment

--- splunklib/modularinput/modularinput_testlib.py
+++ splunklib/modularinput/modularinput_testlib.py
@@ -14,7 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-#Utility file for common functions and imports
+# Utility file for common functions and imports
 
 try:
     import unittest2 as unittest

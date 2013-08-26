065cc7b26eaf9bdccf6fa4281f0488ddc3f0da85
Shakeel Mohamed <smohamed@splunk.com>
2013-08-26 12:39:12 -0700

    Minor docstring changes

--- examples/github_forks/github_forks.py
+++ examples/github_forks/github_forks.py
@@ -110,7 +110,7 @@ class MyScript(Script):
     def stream_events(self, inputs, ew):
         """This function handles all the action: splunk calls this modular input
         without arguments, streams XML describing the inputs to stdin, and waits
-        for XML on stout describing events.
+        for XML on stdout describing events.
 
         If you set use_single_instance to True on the scheme in get_scheme, it
         will pass all the instances of this input to a single instance of this

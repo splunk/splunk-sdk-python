5d205c1919ddf5ce47a4a8a11d7c06d9bd3d93e7
David Noble <dnoble@splunk.com>
2014-02-07 16:47:48 -0800

    Updated copyright notices
    Signed-off-by: David Noble <dnoble@splunk.com>

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2011-2013 Splunk, Inc.
+# Copyright 2011-2014 Splunk, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"): you may
 # not use this file except in compliance with the License. You may obtain

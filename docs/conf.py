f13c8953c49a54cc1a3ac1477a734c9473e7f4f6
David Noble <dnoble@splunk.com>
2014-02-10 17:14:11 -0800

    Updated docs copyright year from 2013 to 2014.
    Signed-off-by: David Noble <dnoble@splunk.com>

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Splunk SDK for Python'
-copyright = u'2013, Splunk Inc'
+copyright = u'2014, Splunk Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

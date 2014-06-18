e4aaca81bfe4420876a8305706298e75b9876973
Henry Zhang <huadianz@gmail.com>
2014-06-18 14:16:16 -0700

    Fix ParseErrors on json objects.

--- splunklib/binding.py
+++ splunklib/binding.py
@@ -38,7 +38,7 @@ from StringIO import StringIO
 
 from contextlib import contextmanager
 
-from xml.etree.ElementTree import XML
+from xml.etree.ElementTree import XML, ParseError
 
 from data import record
 
@@ -888,7 +888,10 @@ class HTTPError(Exception):
         status = response.status
         reason = response.reason
         body = response.body.read()
-        detail = XML(body).findtext("./messages/msg")
+        try:
+            detail = XML(body).findtext("./messages/msg")
+        except ParseError as err:
+            detail = body
         message = "HTTP %d %s%s" % (
             status, reason, "" if detail is None else " -- %s" % detail)
         Exception.__init__(self, _message or message)

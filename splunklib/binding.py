70ec34848a8bd95e21a2a4ac2a7b8c9f67f83971
David Noble <dnoble@splunk.com>
2015-09-20 15:32:30 -0700

    Updated copyright date from 2011-2014 to 2011-2015

--- splunklib/binding.py
+++ splunklib/binding.py
@@ -1,4 +1,4 @@
-# Copyright 2011-2014 Splunk, Inc.
+# Copyright 2011-2015 Splunk, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"): you may
 # not use this file except in compliance with the License. You may obtain

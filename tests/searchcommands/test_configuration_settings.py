acfd7c14d32ffff3432f53be58189b718a0c8fb7
Ke Li <kel@splunk.com>
2016-08-05 16:58:19 +0800

    fix unit test error

--- tests/searchcommands/test_configuration_settings.py
+++ tests/searchcommands/test_configuration_settings.py
@@ -146,7 +146,7 @@ class TestConfigurationSettings(TestCase):
 
         self.assertEqual(
             [(name, value) for name, value in command.configuration.iteritems()],
-            [('type', 'stateful')])
+            [('type', 'streaming')])
 
         self.assertIs(command.configuration.distributed, True)
         self.assertEqual(command.configuration.type, 'streaming')
@@ -165,7 +165,7 @@ class TestConfigurationSettings(TestCase):
 
         self.assertEqual(
             [(name, value) for name, value in command.configuration.iteritems()],
-            [('required_fields', ['field_1', 'field_2', 'field_3']), ('type', 'streaming')])
+            [('required_fields', ['field_1', 'field_2', 'field_3']), ('type', 'stateful')])
 
         return
 

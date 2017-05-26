22c8fdfbc933a32f1a24becb531fb24b9c62372b
Colin Humphreys <colin@encypher.net>
2017-05-26 16:02:55 +1000

    distrubuted commands are streaming

--- tests/searchcommands/test_configuration_settings.py
+++ tests/searchcommands/test_configuration_settings.py
@@ -81,7 +81,7 @@ class TestConfigurationSettings(TestCase):
 
         self.assertIs(command.configuration.distributed, False)
         self.assertIs(command.configuration.generating, True)
-        self.assertEqual(command.configuration.type, 'streaming')
+        self.assertEqual(command.configuration.type, 'stateful')
 
         command.configuration.distributed = True
 
@@ -96,7 +96,7 @@ class TestConfigurationSettings(TestCase):
 
         self.assertEqual(
             [(name, value) for name, value in command.configuration.iteritems()],
-            [('generating', True), ('type', 'stateful')])
+            [('generating', True), ('type', 'streaming')])
 
         return
 

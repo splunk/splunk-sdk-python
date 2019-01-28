0c34f3d08d7218727f0eeb2907067935ddf878d2
Eric Cheng <xcheng@splunk.com>
2019-01-28 15:23:02 -0800

    modify python path

--- tests/searchcommands/test_searchcommands_app.py
+++ tests/searchcommands/test_searchcommands_app.py
@@ -417,7 +417,7 @@ class TestSearchCommandsApp(TestCase):
                             ofile.write(b)
                 with io.open(uncompressed_file, 'rb') as ifile:
                     env = os.environ.copy()
-                    env['PYTHONPATH'] = sys.path
+                    env['PYTHONPATH'] = ":".join(sys.path)
                     # pprint.pprint(env)
                     # pprint.pprint(env['PYTHONPATH'])
                     process = Popen(recording.get_args(command), stdin=ifile, stderr=PIPE, stdout=PIPE, env=env)

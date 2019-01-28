57e9f1445f4116ced813f6febb342203c38905e8
Eric Cheng <xcheng@splunk.com>
2019-01-28 15:19:05 -0800

    update pythonpath

--- tests/searchcommands/test_searchcommands_app.py
+++ tests/searchcommands/test_searchcommands_app.py
@@ -417,8 +417,9 @@ class TestSearchCommandsApp(TestCase):
                             ofile.write(b)
                 with io.open(uncompressed_file, 'rb') as ifile:
                     env = os.environ.copy()
-                    pprint.pprint(env)
-                    pprint.pprint(env['PYTHONPATH'])
+                    env['PYTHONPATH'] = sys.path
+                    # pprint.pprint(env)
+                    # pprint.pprint(env['PYTHONPATH'])
                     process = Popen(recording.get_args(command), stdin=ifile, stderr=PIPE, stdout=PIPE, env=env)
                     output, errors = process.communicate()
                 with io.open(recording.output_file, 'rb') as ifile:

744a9f3755c4b6e76dffeded6d2decb38f704e4f
Shakeel Mohamed <smohamed@splunk.com>
2019-02-25 17:32:29 -0800

    add path to search commands examples

--- examples/searchcommands_template/bin/stream.py
+++ examples/searchcommands_template/bin/stream.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
 from splunklib.searchcommands import \
     dispatch, StreamingCommand, Configuration, Option, validators
 

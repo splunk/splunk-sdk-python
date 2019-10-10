602f984f24801a3b6614d256d580da4ed01b1bc1
rfaircloth-splunk <rfaircloth@splunk.com>
2019-10-09 20:47:11 -0400

    test work

--- setup.py
+++ setup.py
@@ -228,6 +228,8 @@ setup(
                 "splunklib.modularinput",
                 "splunklib.searchcommands"],
 
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+
     url="http://github.com/splunk/splunk-sdk-python",
 
     version=splunklib.__version__,

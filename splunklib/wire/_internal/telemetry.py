a8ba77e63a5feb82c878eca6181496acafec9691
Amy Sutedja <asutedja@splunk.com>
2020-06-22 13:39:06 -0700

    Telemetry internals

---
+++ splunklib/wire/_internal/telemetry.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+#
+# Copyright © 2011-2020 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"): you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from splunklib.wire._internal.json_sink import JsonSink
+
+PATH_TELEMETRY = "telemetry-metric"
+
+class Telemetry(JsonSink):
+    def __init__(self, service, **kwargs):
+        super(Telemetry, self).__init__(service, PATH_TELEMETRY, **kwargs)

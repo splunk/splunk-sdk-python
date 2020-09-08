27f92a54989a20e137ddd9076899f87896acb42a
David Poncelow <dponcelow@splunk.com>
2020-09-08 14:25:05 -0700

    add test for v1 search commands

--- tests/searchcommands/test_multibyte_processing.py
+++ tests/searchcommands/test_multibyte_processing.py
@@ -7,17 +7,21 @@ from os import path
 from splunklib import six
 from splunklib.searchcommands import StreamingCommand, Configuration
 
+
 def build_test_command():
     @Configuration()
     class TestSearchCommand(StreamingCommand):
         def stream(self, records):
             for record in records:
                 yield record
+
     return TestSearchCommand()
+
 
 def get_input_file(name):
     return path.join(
         path.dirname(path.dirname(__file__)), 'data', 'custom_search', name + '.gz')
+
 
 def test_multibyte_chunked():
     data = gzip.open(get_input_file("multibyte_input"))
@@ -25,3 +29,11 @@ def test_multibyte_chunked():
         data = io.TextIOWrapper(data)
     cmd = build_test_command()
     cmd._process_protocol_v2(sys.argv, data, sys.stdout)
+
+
+def test_v1_searchcommand():
+    data = gzip.open(get_input_file("v1_search_input"))
+    if not six.PY2:
+        data = io.TextIOWrapper(data)
+    cmd = build_test_command()
+    cmd._process_protocol_v1(["test_script.py", "__EXECUTE__"], data, sys.stdout)

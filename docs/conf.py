f9191a1430955890c14caa5355265a1ab162e761
PKing70 <39703314+PKing70@users.noreply.github.com>
2021-06-10 10:20:30 -0700

    Update conf.py
    Update copyright to 2021

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Splunk SDK for Python'
-copyright = u'2020, Splunk Inc'
+copyright = u'2021, Splunk Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

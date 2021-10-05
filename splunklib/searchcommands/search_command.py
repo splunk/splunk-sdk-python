d585269646d55b925e99821b379a119f5dd15f3d
vmalaviya <vmalaviya@splunk.com>
2021-10-05 14:49:13 +0530

    Update search_command.py

--- splunklib/searchcommands/search_command.py
+++ splunklib/searchcommands/search_command.py
@@ -1072,7 +1072,7 @@ class SearchCommand(object):
 SearchMetric = namedtuple('SearchMetric', ('elapsed_seconds', 'invocation_count', 'input_count', 'output_count'))
 
 
-def dispatch(command_class, argv=sys.argv, input_file=sys.stdin, output_file=sys.stdout, module_name=None, allow_empty_list = False):
+def dispatch(command_class, argv=sys.argv, input_file=sys.stdin, output_file=sys.stdout, module_name=None, allow_empty_list=False):
     """ Instantiates and executes a search command class
 
     This function implements a `conditional script stanza <https://docs.python.org/2/library/__main__.html>`_ based on the value of

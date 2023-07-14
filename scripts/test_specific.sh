a16129dd7a90570bea6c9654dd8be1721f54d279
akaila-splunk <akaila@splunk.com>
2023-07-14 18:06:27 +0530

    release v1.7.4 changes
    - update readme and test_specific command
    - update GH CI workflow wrt dependabot PR

--- scripts/test_specific.sh
+++ scripts/test_specific.sh
@@ -1,2 +1,4 @@
 echo "To run a specific test:"
-echo "  tox -e py27,py37 [test_file_path]::[test_name]"
+echo "  tox -e py27,py37 [test_file_path]::[TestClassName]::[test_method]"
+echo "For Example, To run 'test_autologin' testcase from 'test_service.py' file run"
+echo "  tox -e py37 -- tests/test_service.py::ServiceTestCase::test_autologin"

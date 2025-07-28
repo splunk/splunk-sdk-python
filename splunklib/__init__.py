69bb731ea22b18671c00aab64bf4cd64fbab8304
Bartosz <bjedrecki@splunk.com>
2025-07-28 21:59:09 +0200

    Move versioning to pyproject.toml, adjust version extracting to use importlib

--- splunklib/__init__.py
+++ splunklib/__init__.py
@@ -14,21 +14,29 @@
 
 """Python library for Splunk."""
 
+# Assure backport for Python <3.9
+try:
+    from importlib.metadata import version as importlib_metadata_version
+except (ImportError, ModuleNotFoundError):
+    from importlib_metadata import version as importlib_metadata_version
+
 import logging
 
-DEFAULT_LOG_FORMAT = '%(asctime)s, Level=%(levelname)s, Pid=%(process)s, Logger=%(name)s, File=%(filename)s, ' \
-                     'Line=%(lineno)s, %(message)s'
-DEFAULT_DATE_FORMAT = '%Y-%m-%d %H:%M:%S %Z'
+DEFAULT_LOG_FORMAT = (
+    "%(asctime)s, Level=%(levelname)s, Pid=%(process)s, Logger=%(name)s, File=%(filename)s, "
+    "Line=%(lineno)s, %(message)s"
+)
+DEFAULT_DATE_FORMAT = "%Y-%m-%d %H:%M:%S %Z"
 
 
 # To set the logging level of splunklib
 # ex. To enable debug logs, call this method with parameter 'logging.DEBUG'
 # default logging level is set to 'WARNING'
-def setup_logging(level, log_format=DEFAULT_LOG_FORMAT, date_format=DEFAULT_DATE_FORMAT):
-    logging.basicConfig(level=level,
-                        format=log_format,
-                        datefmt=date_format)
+def setup_logging(
+    level, log_format=DEFAULT_LOG_FORMAT, date_format=DEFAULT_DATE_FORMAT
+):
+    logging.basicConfig(level=level, format=log_format, datefmt=date_format)
 
 
-__version_info__ = (2, 1, 0)
-__version__ = ".".join(map(str, __version_info__))
+# Extract current package version from pyproject.toml
+__version__: str = importlib_metadata_version("splunk-sdk")

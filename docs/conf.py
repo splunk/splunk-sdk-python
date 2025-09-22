fd3a21886fabffb69b116b1f8349844727cc38fd
Bartosz Jędrecki <bjedrecki@splunk.com>
2025-09-22 17:29:23 +0200

    Fix "Converting `source_suffix = '.rst'` to `source_suffix = {'.rst': 'restructuredtext'}`."

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.ifconfig"]
 templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = ".rst"
+source_suffix = {".rst": "restructuredtext"}
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'

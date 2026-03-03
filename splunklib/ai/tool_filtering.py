75899b024868e943d200eedcb782be78b6230652
Bartosz Jędrecki <bjedrecki@splunk.com>
2026-03-03 14:52:00 +0100

    Adjustments in tool_filtering.py (#71)

--- splunklib/ai/tool_filtering.py
+++ splunklib/ai/tool_filtering.py
@@ -1,3 +1,17 @@
+# Copyright © 2011-2026 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"): you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 from collections.abc import Sequence
 from dataclasses import dataclass
 
@@ -6,23 +20,21 @@ from splunklib.ai.tools import Tool
 
 @dataclass(frozen=True)
 class ToolFilters:
+    """Allowlists by which Tools are filtered."""
+
     allowed_names: Sequence[str]
     allowed_tags: Sequence[str]
 
 
+def _is_allowed(tool: Tool, filters: ToolFilters) -> bool:
+    return (
+        tool.name in filters.allowed_names
+        or len(set(filters.allowed_tags).intersection(tool.tags or [])) > 0
+    )
+
+
 def filter_tools(tools: Sequence[Tool], filters: ToolFilters) -> list[Tool]:
-    """Filters all tools by allowlists provided by user to the Agent
+    """Filters all tools by allowlists provided by user to the Agent."""
 
-    TODO: What happens when local and remote tools share names?
-    Does local overwrite remote (or vice versa)? Do we allow choice between overwriting,
-    prefixing both or raising exceptions? See tools.py:load_mcp_tools()
-    """
-
-    def _predicate(tool: Tool) -> bool:
-        return (
-            tool.name in filters.allowed_names
-            or len(set(filters.allowed_tags).intersection(tool.tags or [])) > 0
-        )
-
-    filtered_tools = list(filter(_predicate, tools))
+    filtered_tools = [t for t in tools if _is_allowed(t, filters)]
     return filtered_tools

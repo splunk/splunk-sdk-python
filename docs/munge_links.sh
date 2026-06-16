6777261d67c5e0356fc1cfe187ea065f5a53af45
Bartosz Jędrecki <bjedrecki@splunk.com>
2026-06-16 14:32:52 +0200

    Fix links in CSS and READMEs (#805)

--- docs/munge_links.sh
+++ docs/munge_links.sh
@@ -4,5 +4,6 @@ TARGET=$1
 
 for file in $TARGET/*.html; do
     echo ${file}
-    sed -i -e 's/class="reference external"/class="reference external" target="_blank"/g' "${file}"
+    # we use perl instead of sed - macOS sed -i creates backup files, but -i '' makes it not work on Linux
+    perl -i -pe 's/class="reference external"/class="reference external" target="_blank"/g' "${file}"
 done

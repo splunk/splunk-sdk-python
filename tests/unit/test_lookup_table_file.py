aedce024b754b6ef1f88f74122698b22c77c574c
Danny <dannyontheair@outlook.com>
2026-06-29 07:59:01 +0700

    Add LookupTableFile entity and LookupTableFiles collection

---
+++ tests/unit/test_lookup_table_file.py
@@ -0,0 +1,34 @@
+# Copyright © 2011-2026 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"): you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from splunklib.client import (
+    PATH_LOOKUP_TABLE_FILES,
+    Collection,
+    Entity,
+    LookupTableFile,
+    LookupTableFiles,
+)
+
+
+class TestLookupTableFile:
+    def test_is_entity_subclass(self) -> None:
+        assert issubclass(LookupTableFile, Entity)
+
+    def test_path_constant(self) -> None:
+        assert PATH_LOOKUP_TABLE_FILES == "data/lookup-table-files/"
+
+
+class TestLookupTableFiles:
+    def test_is_collection_subclass(self) -> None:
+        assert issubclass(LookupTableFiles, Collection)
